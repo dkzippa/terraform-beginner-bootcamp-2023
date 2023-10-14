331303038ed230e6bfbc3e7ac88000dfa4378746
Andrew Ermeneu <dkzippa@gmail.com>
2023-10-14 08:26:59 +0000

    implement cdn

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -7,3 +7,19 @@ output "website_endpoint" {
   description = "endpoint for static website"
   value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 }
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
---
+++ modules/terrahouse_aws/res-storage.tf
@@ -0,0 +1,86 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "TF Bootcamp test bucket"
+    Environment = "Dev"
+    UserUid     = var.user_uuid
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"  
+  }
+
+  error_document {
+    key = "error.html"  
+  }
+}
+
+
+resource "aws_s3_object" "upload_index" { 
+  key = "index.html"
+  bucket = aws_s3_bucket.website_bucket.bucket
+  source = "${path.cwd}/public/index.html"
+  content_type = "text/html"
+  etag = filemd5("${path.cwd}/public/index.html")
+  count = fileexists("${path.root}/public/index.html") ? 1 : 0
+}
+
+resource "aws_s3_object" "upload_error" { 
+  key = "error.html"
+  bucket = aws_s3_bucket.website_bucket.bucket
+  source = "${path.root}/public/error.html"
+  content_type = "text/html"
+  etag = filemd5("${path.root}/public/error.html")
+  count = fileexists("${path.root}/public/error.html") ? 1 : 0
+}
+
+# resource "aws_s3_bucket_policy" "bucket_policy" {
+#   bucket = aws_s3_bucket.website_bucket.id
+# #   policy = jsonencode({
+# #     {
+
+# #     }
+# #   })
+
+# policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {     
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": ["s3:GetObject"],
+#       "Resource": ["${aws_s3_bucket.website_bucket.arn}/*"]
+#     }
+#   ]
+# }
+# POLICY
+# }
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+  policy = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = {
+      "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
+      "Effect" = "Allow",
+      "Principal" = {
+        "Service" = "cloudfront.amazonaws.com"
+      },
+      "Action" = "s3:GetObject",
+      "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+      "Condition" = {
+      "StringEquals" = {
+          #"AWS:SourceArn": data.aws_caller_identity.current.arn
+          "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+        }
+      }
+    }
+  })
+}
